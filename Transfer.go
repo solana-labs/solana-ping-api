5922efbbb3ef956ee1423d55158ebc9ce85498e3
rachaelpai <komimi.p@gmail.com>
2022-12-16 17:12:38 +0000

    add a condition to avoid empty response

--- Transfer.go
+++ Transfer.go
@@ -138,7 +138,7 @@ func waitConfirmation(c *client.Client, txHash string, timeout time.Duration, re
 			}
 		}
 		if now.Sub(elapse).Seconds() > timeout.Seconds() {
-			if *resp.ConfirmationStatus == rpc.CommitmentProcessed {
+			if resp != nil && *resp.ConfirmationStatus == rpc.CommitmentProcessed {
 				return PingResultError(ErrInProcessedStateTimeout.Error())
 			}
 			return PingResultError(ErrWaitForConfirmedTimeout.Error())
